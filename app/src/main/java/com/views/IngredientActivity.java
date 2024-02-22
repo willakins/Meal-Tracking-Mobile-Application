ad770f60c41b8745085254ccfc5fbccda1093403
corbettryan <ryanmobile@thecorbetts.org>
2024-02-22 10:17:43 -0500

    added xml files for placeholder screens. Implemented basic onCreate methods in view files.

--- app/src/main/java/com/views/IngredientActivity.java
+++ app/src/main/java/com/views/IngredientActivity.java
@@ -1,4 +1,23 @@
 package com.views;
 
-public class IngredientActivity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IngredientActivity extends AppCompatActivity {
+
+    private TextView IngredientTextView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_ingredient);
+        IngredientTextView = findViewById(R.id.IngredientTextView);
+
+    }
 }
--- app/src/main/java/com/views/ShoppingActivity.java
+++ app/src/main/java/com/views/ShoppingActivity.java
@@ -1,4 +1,23 @@
 package com.views;
 
-public class ShoppingActivity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShoppingActivity extends AppCompatActivity {
+
+    private TextView ShoppingTextView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_shopping);
+        ShoppingTextView = findViewById(R.id.ShoppingTextView);
+
+    }
 }
