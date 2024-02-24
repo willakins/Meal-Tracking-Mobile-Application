a722ca57d017d40d9c4d92b5c6930ed58a8ea77b
willakins <willakins23@gmail.com>
2024-02-24 12:50:23 -0500

    Modified login screen as well as connecting android manifest

--- app/src/main/java/com/views/LoginActivity.java
+++ app/src/main/java/com/views/LoginActivity.java
@@ -2,6 +2,7 @@ package com.views;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,14 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     //Has a login button that changes to HomeActivity
     //Has a button that changes to AccountCreateActivity
     //Null and whitespace checks for text fields
-    private EditText usernameEditText;
+    //private EditText usernameEditText;
+    private ImageView background;
+    /**
     private EditText passwordEditText;
     private Button loginButton;
-
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        background = findViewById(R.id.backgroundImage);
 
         /**
         usernameEditText = findViewById(R.id.usernameText);
