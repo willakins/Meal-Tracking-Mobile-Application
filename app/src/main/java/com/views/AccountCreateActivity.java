e8e2ef6e0c63d72d2b80381138b60eaafab7a1aa
willakins <willakins23@gmail.com>
2024-02-25 09:58:13 -0500

    Added create account page

--- app/src/main/java/com/views/AccountCreateActivity.java
+++ app/src/main/java/com/views/AccountCreateActivity.java
@@ -1,5 +1,9 @@
 package com.views;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AccountCreateActivity extends AppCompatActivity {
@@ -8,4 +12,18 @@ public class AccountCreateActivity extends AppCompatActivity {
     //Connects to database
     //Null and whitespace checks for textfields
     //Moves to HomeActivity on successful account creation
+    private EditText newUsername;
+    private EditText newPassword;
+    private Button createButton;
+    private Button quitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        newPassword = findViewById(R.id.editTextTextUsername);
+        newUsername = findViewById(R.id.editTextTextUsername);
+        createButton = findViewById(R.id.createAccountButton);
+        quitButton = findViewById(R.id.quitButton);
+    }
 }
