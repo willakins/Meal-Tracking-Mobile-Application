74f3d02a53392270b28d0f96ef03ce15c0c72a4b
corbettryan <ryanmobile@thecorbetts.org>
2024-03-09 10:51:53 -0500

    Changed button names, and added names to unit tests.

--- app/src/test/java/com/example/UnitTestThree.java
+++ app/src/test/java/com/example/UnitTestThree.java
@@ -7,12 +7,13 @@ import org.junit.Test;
  * do not start writing the test until you change author name and do a pull request.
  * additionally, don't start writing your pull request until you pull from main
  * so that you don't cause a merge conflict
- * @author
+ * @author Ryan Corbett
  */
 public class UnitTestThree {
 
     @Test
-    public void testInsertNameHere() {
+    public void testPieChartData1() {
+
         
     }
 }
