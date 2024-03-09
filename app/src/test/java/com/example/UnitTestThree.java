3db9a2d3eca5ad8b64dbf1fea012d4bbb2d5cd8d
corbettryan <ryanmobile@thecorbetts.org>
2024-03-09 13:55:48 -0500

    added method to calculate caloric deficit and updated unit tests

--- app/src/test/java/com/example/UnitTestThree.java
+++ app/src/test/java/com/example/UnitTestThree.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import com.model.User;
+
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * File placeholder for testing
@@ -12,8 +15,18 @@ import org.junit.Test;
 public class UnitTestThree {
 
     @Test
-    public void testPieChartData1() {
+    public void testCaloricDeficit() {
+        User testUser = new User();
+        int testHeight = 71;
+        int testWeight = 175;
+        testUser.setHeight(testHeight);
+        testUser.setWeight(testWeight);
+        testUser.setIsMale(true);
+        testUser.setCalorieGoal(testUser.calculateCalorieGoal());
+        testUser.setCaloriesToday(1500);
+        int deficit = testUser.getCalorieGoal() - testUser.getCaloriesToday();
 
+        assertEquals(testUser.getCaloricDeficit(), deficit);
         
     }
 }
