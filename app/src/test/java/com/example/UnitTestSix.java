6a823338361b4516d4c3c03747bd3f1396773ac5
jessica-kalloor <jessica.kalloor@gmail.com>
2024-03-09 21:02:47 -0500

    testing illegal arguments

--- app/src/test/java/com/example/UnitTestSix.java
+++ app/src/test/java/com/example/UnitTestSix.java
@@ -14,4 +14,13 @@ import org.junit.Test;
  * @author Jessica Kalloor
  */
 public class UnitTestSix {
+    @Test(expected = IllegalArgumentException.class)
+    public void testMealConstructor_NegativeCalories() {
+        Meal meal = new Meal("Snack", -100);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMealConstructor_EmptyName() {
+        Meal meal = new Meal("", 200);
+    }
 }
