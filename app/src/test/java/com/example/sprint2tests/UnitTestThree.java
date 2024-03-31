97b6d86d5a0aab7e6d1d94e82736b494848c0644
corbettryan <ryanmobile@thecorbetts.org>
2024-03-31 09:54:32 -0400

    moved unit tests to dedicated class

--- app/src/test/java/com/example/sprint2tests/UnitTestThree.java
+++ app/src/test/java/com/example/sprint2tests/UnitTestThree.java
@@ -35,34 +35,5 @@ public class UnitTestThree {
         
     }
 
-    @Test
-    public void testRecipeName() {
-        String RecipeName = "Test Recipe";
-        ArrayList<Ingredient > ingredients = new ArrayList<Ingredient>();
-        ingredients.add(new Ingredient("test ingredient", "1"));
-        ingredients.add(new Ingredient("test ingredient", "1"));
-        ingredients.add(new Ingredient("test ingredient", "1"));
-        Recipe recipe = new Recipe(RecipeName, ingredients);
-
-        assertEquals(recipe.getName(), "Test Recipe");
-
-
-    }
-
-    @Test
-    public void testRecipeIngredients() {
-        String RecipeName = "Test Recipe";
-        ArrayList<Ingredient > ingredients = new ArrayList<Ingredient>();
-        ingredients.add(new Ingredient("test ingredient", "1"));
-        ingredients.add(new Ingredient("test ingredient", "2"));
-        ingredients.add(new Ingredient("test ingredient", "3"));
-        Recipe recipe = new Recipe(RecipeName, ingredients);
-
-        for (int i = 0; i < ingredients.size(); i++) {
-            assertEquals("test ingredient", recipe.getIngredients().get(i).getName());
-            assertEquals(Integer.toString(i + 1), recipe.getIngredients().get(i).getQuantity());
-        }
-
-    }
 
 }
