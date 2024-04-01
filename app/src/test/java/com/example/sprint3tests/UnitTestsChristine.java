8801611621ac9efd39952634bd5a2e5ab250d00e
clee829 <unis4christine@gmail.com>
2024-03-31 23:40:17 -0400

    Created unit tests

--- app/src/test/java/com/example/sprint3tests/UnitTestsChristine.java
+++ app/src/test/java/com/example/sprint3tests/UnitTestsChristine.java
@@ -1,5 +1,11 @@
 package com.example.sprint3tests;
 
+import static org.junit.Assert.assertEquals;
+
+import com.model.Ingredient;
+import com.model.User;
+import com.viewmodels.PantryViewModel;
+
 import org.junit.Test;
 
 /**
@@ -12,7 +18,39 @@ import org.junit.Test;
  */
 public class UnitTestsChristine {
     @Test
-    public void testInsertNameHere() {
+    public void testDuplicateIngredients() {
+        PantryViewModel pantryViewModel = PantryViewModel.getInstance();
+        User testUser = new User();
+        pantryViewModel.setUser(testUser);
 
+        // Test Ingredient Details
+        String name = "Butter";
+        String quantity = "100g";
+        String calories = "30";
+        String expiration = "";
+
+        // Add Ingredient
+        pantryViewModel.addIngredient(name, quantity, calories, expiration);
+
+        // Second Test Ingredient Details
+        String secondName = "Butter";
+        String secondQuantity = "100g";
+        String secondCalories = "30";
+        String secondExpiration = "";
+
+        //Add Second Ingredient
+        int result = pantryViewModel.addIngredient(secondName, secondQuantity, secondCalories,
+                secondExpiration);
+
+        // Check if the second ingredient failed
+        assertEquals(5, result);
     }
+
+    @Test
+    public void testIngredientExpiration() {
+        Ingredient testIngredient = new Ingredient("Butter", "1",
+                "30", "20");
+        assertEquals("20", testIngredient.getExpiration());
+    }
+
 }
