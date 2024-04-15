3cb315fdeae3039ecd9ee5f07517f0a29c262acf
willakins <willakins23@gmail.com>
2024-04-15 13:29:25 -0400

    Adding 2nd design pattern.

--- app/src/main/java/com/views/RecipeFragment.java
+++ app/src/main/java/com/views/RecipeFragment.java
@@ -1,6 +1,7 @@
 package com.views;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -15,6 +16,7 @@ import android.widget.EditText;
 
 import com.model.Ingredient;
 import com.model.StrategySprint4.Recipe;
+import com.model.ShoppingItem;
 import com.model.Strategy.RecipeContext;
 import com.model.User;
 import com.viewmodels.CookBookViewModel;
@@ -30,6 +32,7 @@ public class RecipeFragment extends Fragment {
     private static CookBookViewModel cookBook;
     private static PantryViewModel pantry;
     private Button submitRecipeButton;
+    private Button goShoppingButton;
     private EditText editTextRecipeName;
     private EditText editTextIngredients;
     private HomeActivity currentContext;
@@ -39,6 +42,7 @@ public class RecipeFragment extends Fragment {
     private RecipeContext recipeContext = new RecipeContext((recipes, pantry) -> recipes);
     private Button sortNameButton;
     private Button sortIngredientsButton;
+    private Button cookRecipeButton;
 
 
     public RecipeFragment() {
@@ -66,6 +70,7 @@ public class RecipeFragment extends Fragment {
         sortIngredientsButton = view.findViewById(R.id.filterButton);
         editTextRecipeName = view.findViewById(R.id.editTextRecipeName);
         editTextIngredients = view.findViewById(R.id.editTextIngredients);
+//        cookRecipeButton = view.findViewById(R.id.cookRecipeButton);
 
         ArrayList<Recipe> recipes = userViewModel.getUser().getCookBook();
         recipesRecyclerView = view.findViewById(R.id.recipesRecyclerView);
@@ -73,6 +78,7 @@ public class RecipeFragment extends Fragment {
         recipeAdapter = new RecipeAdapter(recipes, userViewModel.getUser()
                 .getPantry(), getContext(), recipe -> openRecipeDetails(recipe));
         recipesRecyclerView.setAdapter(recipeAdapter);
+        goShoppingButton = view.findViewById(R.id.buttonGoShopping);
 
         submitRecipeButton.setOnClickListener(v -> {
             cookBook.addRecipe(getContext(), editTextRecipeName, editTextIngredients);
@@ -91,6 +97,35 @@ public class RecipeFragment extends Fragment {
         sortIngredientsButton.setOnClickListener(v -> {
             applyFilterStrategy();
         });
+
+        goShoppingButton.setOnClickListener(v -> {
+            String quantityNeeded = "";
+            int currentQuantity = 0;
+            ArrayList<Recipe> checkedItems = recipeAdapter.getCheckedItems();
+            for (int i = 0; i < checkedItems.size(); i++) {
+                Recipe recipe = checkedItems.get(i);
+                for (int j = 0; j < recipe.getIngredients().size(); j++) {
+                    Ingredient ingredient = recipe.getIngredients().get(j);
+                    int pantryIndex = userViewModel.getUser().locateIngredient(ingredient);
+                    if (pantryIndex == -1) {
+                        currentQuantity = 0;
+                    } else {
+                        currentQuantity = Integer.parseInt(userViewModel.getUser().getPantry()
+                                .get(pantryIndex).getQuantity());
+                    }
+                    int quantityRequired = Integer.parseInt(ingredient.getQuantity());
+                    if (currentQuantity < quantityRequired) {
+                        quantityNeeded = Integer.toString(quantityRequired - currentQuantity);
+                    }
+                    userViewModel.addShoppingItem(ingredient.getName(),
+                            quantityNeeded, "100");
+                }
+            }
+            generateNewAdapter();
+
+
+        });
+
         return view;
     }
 
@@ -156,9 +191,60 @@ public class RecipeFragment extends Fragment {
     private void openRecipeDetails(Recipe recipe) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setTitle(recipe.getName());
-        builder.setMessage("Total Calories: " + recipe.getCalories());
-        builder.setPositiveButton("Close", (dialog, which) -> dialog.dismiss());
+        StringBuilder message = new StringBuilder("Total Calories: " + recipe.getCalories()
+                + "\n" + "Ingredients Required:\n");
+        for (Ingredient ingredient : recipe.getIngredients()) {
+            message.append("- ").append(ingredient.getQuantity()).append(" ")
+                    .append(ingredient.getName()).append("\n");
+        }
+        builder.setMessage(message.toString());
+        builder.setNegativeButton("Close", (dialog, which) -> dialog.dismiss());
+        builder.setPositiveButton("Cook", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Call a function to remove ingredients from the user's account
+                cookRecipe(recipe);
+
+                // Dismiss the dialog
+                dialog.dismiss();
+            }
+        });
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    public void generateNewAdapter() {
+        recipeAdapter = new RecipeAdapter(userViewModel.getUser().getCookBook(),
+                userViewModel.getUser().getPantry(), getContext(),
+                recipe -> openRecipeDetails(recipe));
+        recipesRecyclerView.setAdapter(recipeAdapter);
+    }
+    private void cookRecipe(Recipe recipe) {
+        ArrayList<Ingredient> usedIngredients = recipe.getIngredients();
+        User user = userViewModel.getUser();
+        ArrayList<Ingredient> pantry = user.getPantry();
+        for (Ingredient recipeIngredient : usedIngredients) {
+            // Check if the recipe ingredient exists in the user's pantry
+            for (Ingredient pantryIngredient : pantry) {
+                if (pantryIngredient.getName().equals(recipeIngredient.getName())) {
+                    // Convert the quantity from String to int
+                    int pantryQuantity = Integer.parseInt(pantryIngredient.getQuantity());
+                    int recipeQuantity = Integer.parseInt(recipeIngredient.getQuantity());
+
+                    // Decrement the quantity of the ingredient in the pantry
+                    pantryQuantity -= recipeQuantity;
+
+                    // If the quantity becomes zero, remove the ingredient from the pantry
+                    if (pantryQuantity <= 0) {
+                        pantry.remove(pantryIngredient);
+                    } else {
+                        // Update the quantity in the pantryIngredient object
+                        pantryIngredient.setQuantity(String.valueOf(pantryQuantity));
+                    }
+                    break; // Exit the loop since the ingredient has been found and updated
+                }
+            }
+        }
+    }
+
 }
