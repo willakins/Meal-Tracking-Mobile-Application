9d0b4c5bc1756f127ecba98a8170dcbda9475631
Jace Walden <91030155+TheBlueSP1R1T@users.noreply.github.com>
2024-04-16 09:53:37 -0400

    Revert "Christine Lee unit test 1"

--- app/src/test/java/com/example/sprint4tests/UnitTestsChristine.java
+++ app/src/test/java/com/example/sprint4tests/UnitTestsChristine.java
@@ -1,13 +1,6 @@
 package com.example.sprint4tests;
 
-import static org.junit.Assert.assertEquals;
-
-import com.model.ShoppingItem;
-import com.model.User;
-
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 /**
  * File placeholder for testing
@@ -19,35 +12,7 @@ import java.util.ArrayList;
  */
 public class UnitTestsChristine {
     @Test
-    public void testRemoveItem() {
-        /**
-         * adding the items
-         */
-        String testUsername = "unis4christine@gmail.com";
-        String testPassword = "password";
-        User testUser = new User(testUsername, testPassword);
-        ArrayList<ShoppingItem> shoppingList = new ArrayList<>();
-        ShoppingItem item1 = new ShoppingItem("Pasta", "2", "20");
-        ShoppingItem item2 = new ShoppingItem("Cheese", "3", "80");
-        ShoppingItem item3 = new ShoppingItem("Chicken", "4", "100");
-        ShoppingItem item4 = new ShoppingItem("Butter", "1", "30");
-        shoppingList.add(item1);
-        shoppingList.add(item2);
-        shoppingList.add(item3);
-        shoppingList.add(item4);
-
-        /**
-         * testing size
-         */
-        testUser.setShoppingList(shoppingList);
-        assertEquals(4, testUser.getShoppingList().size());
-
-        /**
-         * removing item
-         */
-        shoppingList.remove(item3);
-        testUser.setShoppingList(shoppingList);
-        assertEquals(3, testUser.getShoppingList().size());
+    public void testOne() {
 
     }
 
